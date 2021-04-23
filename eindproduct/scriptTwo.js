014ff98a6fbf3f335b53822ac5bd944fec0ca899
NathanNeelis <nathan.neelis@hva.nl>
2021-04-23 16:39:55 +0200

    final update

--- eindproduct/scriptTwo.js
+++ eindproduct/scriptTwo.js
@@ -1,10 +1,13 @@
 const contentBox = document.querySelector('.box')
 const startBtn = document.getElementById('start')
+const pauseBtn = document.getElementById('pause')
+const resetBtn = document.getElementById('reset')
 
 const vasilisIcon = document.querySelector('.roundVasilis')
 const espenIcon = document.querySelector('.roundEspen')
 
-var spns = document.getElementsByTagName("SPAN");
+// var spns = document.getElementsByTagName("SPAN");
+let spns = document.querySelectorAll('.box span')
 var audi = document.getElementById("adi");
 
 audi.addEventListener("timeupdate", read);
@@ -29,6 +32,8 @@ const espenTHL = document.querySelector('.roundEspen #EspenTHL')
 const espenFV = document.querySelector('.roundEspen #EspenFV')
 const espenQW = document.querySelector('.roundEspen #EspenQW')
 
+const highlight = "rgb(252, 186, 3)"
+const highlightO = "rgba(252, 186, 3, 0.3)"
 
 espenSmile.classList.add('showItem')
 
@@ -38,8 +43,8 @@ function read() {
     for (i = 0; i < spns.length; i++) {
         var time = Number(spns[i].id.slice(2));
         if (time < audi.currentTime) {
-            if (i > 0) spns[i - 1].style.backgroundColor = "white";
-            spns[i].style.backgroundColor = "red";
+            if (i > 0) spns[i - 1].style.backgroundColor = highlightO;
+            spns[i].style.backgroundColor = highlight;
         }
     }
 
@@ -47,7 +52,7 @@ function read() {
     for (i = 0; i < spns.length; i++) {
         var time = Number(spns[i].id.slice(2));
         if (time < audi.currentTime) {
-            if (spns[i].style.backgroundColor === "red") {
+            if (spns[i].style.backgroundColor === highlight) {
                 spns[i].scrollIntoView({
                     behavior: "smooth",
                     block: "center",
@@ -57,20 +62,52 @@ function read() {
         }
     }
 
-    // ANIMATE ICONS ON SPEAKER 
+    // ANIMATE NUANCES
     for (i = 0; i < spns.length; i++) {
         var time = Number(spns[i].id.slice(2));
         if (time < audi.currentTime) {
-            if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("vasilis")) {
-                console.log('Animate vasilis icon')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("espen")) {
-                console.log('Animate espen icon')
+            if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("thumble")) {
+                // ADD THUMBLE WEED -> desktop only
+                const thumbleWeed = document.querySelector('.thumbleWeed')
+                thumbleWeed.classList.add('showTW')
+                thumbleWeed.classList.add('rollTW')
+
+                // hide thumbleweed
+                const thumbleWeedActive = document.querySelector('.showTW')
+
+                if (thumbleWeedActive) {
+
+                    setTimeout(function () {
+
+                        // const thumbleWeed = document.querySelector('.thumbleWeed')
+                        thumbleWeed.classList.remove('showTW')
+                        thumbleWeed.classList.remove('rollTW')
+
+                        // thumbleWeed.style.Opacity = 0;
+                    }, 7000)
+                }
+                // console.log('thumble weed')
+
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("suprised")) {
+                // ADD FRIENDS GIF SUPRISED -> Desktop only
+                const friendsGif = document.querySelector('.friendsSuprised')
+                friendsGif.classList.add('showFriends')
+                setTimeout(function () {
+                    friendsGif.classList.remove('showFriends')
+                }, 5000)
+
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("mushroom")) {
+                // ADD Mario mushroom
+                const mario = document.querySelector('.mario')
+                mario.classList.add('run')
+
+                setTimeout(function () {
+                    mario.classList.remove('run')
+                }, 9000)
             }
         }
 
     }
-
-
 
     // showItem class
     // st4 to hide
@@ -80,7 +117,7 @@ function read() {
     for (i = 0; i < spns.length; i++) {
         var time = Number(spns[i].id.slice(2));
         if (time < audi.currentTime) {
-            if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("smile") && spns[i].classList.contains("vasilis")) {
+            if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("smile") && spns[i].classList.contains("vasilis")) {
                 smile.classList.add('showItem')
                 AEI.classList.remove('showItem')
                 Neutral.classList.remove('showItem')
@@ -88,8 +125,8 @@ function read() {
                 THL.classList.remove('showItem')
                 FV.classList.remove('showItem')
                 QW.classList.remove('showItem')
-                console.log('smile')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("aei") && spns[i].classList.contains("vasilis")) {
+                // console.log('smile')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("aei") && spns[i].classList.contains("vasilis")) {
                 smile.classList.remove('showItem')
                 AEI.classList.add('showItem')
                 Neutral.classList.remove('showItem')
@@ -97,8 +134,8 @@ function read() {
                 THL.classList.remove('showItem')
                 FV.classList.remove('showItem')
                 QW.classList.remove('showItem')
-                console.log('aei mouth')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("neutral") && spns[i].classList.contains("vasilis")) {
+                // console.log('aei mouth')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("neutral") && spns[i].classList.contains("vasilis")) {
                 smile.classList.remove('showItem')
                 AEI.classList.remove('showItem')
                 Neutral.classList.add('showItem')
@@ -106,8 +143,8 @@ function read() {
                 THL.classList.remove('showItem')
                 FV.classList.remove('showItem')
                 QW.classList.remove('showItem')
-                console.log('Neutral mouth')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("oMouth") && spns[i].classList.contains("vasilis")) {
+                // console.log('Neutral mouth')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("oMouth") && spns[i].classList.contains("vasilis")) {
                 smile.classList.remove('showItem')
                 AEI.classList.remove('showItem')
                 Neutral.classList.remove('showItem')
@@ -115,8 +152,8 @@ function read() {
                 THL.classList.remove('showItem')
                 FV.classList.remove('showItem')
                 QW.classList.remove('showItem')
-                console.log('o mouth')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("thl") && spns[i].classList.contains("vasilis")) {
+                // console.log('o mouth')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("thl") && spns[i].classList.contains("vasilis")) {
                 smile.classList.remove('showItem')
                 AEI.classList.remove('showItem')
                 Neutral.classList.remove('showItem')
@@ -124,8 +161,8 @@ function read() {
                 THL.classList.add('showItem')
                 FV.classList.remove('showItem')
                 QW.classList.remove('showItem')
-                console.log('THL mouth')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("fv") && spns[i].classList.contains("vasilis")) {
+                // console.log('THL mouth')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("fv") && spns[i].classList.contains("vasilis")) {
                 smile.classList.remove('showItem')
                 AEI.classList.remove('showItem')
                 Neutral.classList.remove('showItem')
@@ -133,8 +170,8 @@ function read() {
                 THL.classList.remove('showItem')
                 FV.classList.add('showItem')
                 QW.classList.remove('showItem')
-                console.log('FV mouth')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("qw") && spns[i].classList.contains("vasilis")) {
+                // console.log('FV mouth')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("qw") && spns[i].classList.contains("vasilis")) {
                 smile.classList.remove('showItem')
                 AEI.classList.remove('showItem')
                 Neutral.classList.remove('showItem')
@@ -142,8 +179,8 @@ function read() {
                 THL.classList.remove('showItem')
                 FV.classList.remove('showItem')
                 QW.classList.add('showItem')
-                console.log('QW mouth')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("smile") && spns[i].classList.contains("espen")) {
+                // console.log('QW mouth')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("smile") && spns[i].classList.contains("espen")) {
                 espenSmile.classList.add('showItem')
                 espenAEI.classList.remove('showItem')
                 espenNeutral.classList.remove('showItem')
@@ -151,8 +188,8 @@ function read() {
                 espenTHL.classList.remove('showItem')
                 espenFV.classList.remove('showItem')
                 espenQW.classList.remove('showItem')
-                console.log('smile')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("aei") && spns[i].classList.contains("espen")) {
+                // console.log('smile')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("aei") && spns[i].classList.contains("espen")) {
                 espenSmile.classList.remove('showItem')
                 espenAEI.classList.add('showItem')
                 espenNeutral.classList.remove('showItem')
@@ -160,8 +197,8 @@ function read() {
                 espenTHL.classList.remove('showItem')
                 espenFV.classList.remove('showItem')
                 espenQW.classList.remove('showItem')
-                console.log('aei mouth')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("neutral") && spns[i].classList.contains("espen")) {
+                // console.log('aei mouth')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("neutral") && spns[i].classList.contains("espen")) {
                 espenSmile.classList.remove('showItem')
                 espenAEI.classList.remove('showItem')
                 espenNeutral.classList.add('showItem')
@@ -169,8 +206,8 @@ function read() {
                 espenTHL.classList.remove('showItem')
                 espenFV.classList.remove('showItem')
                 espenQW.classList.remove('showItem')
-                console.log('Neutral mouth')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("oMouth") && spns[i].classList.contains("espen")) {
+                // console.log('Neutral mouth')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("oMouth") && spns[i].classList.contains("espen")) {
                 espenSmile.classList.remove('showItem')
                 espenAEI.classList.remove('showItem')
                 espenNeutral.classList.remove('showItem')
@@ -178,8 +215,8 @@ function read() {
                 espenTHL.classList.remove('showItem')
                 espenFV.classList.remove('showItem')
                 espenQW.classList.remove('showItem')
-                console.log('o mouth')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("thl") && spns[i].classList.contains("espen")) {
+                // console.log('o mouth')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("thl") && spns[i].classList.contains("espen")) {
                 espenSmile.classList.remove('showItem')
                 espenAEI.classList.remove('showItem')
                 espenNeutral.classList.remove('showItem')
@@ -187,8 +224,8 @@ function read() {
                 espenTHL.classList.add('showItem')
                 espenFV.classList.remove('showItem')
                 espenQW.classList.remove('showItem')
-                console.log('THL mouth')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("fv") && spns[i].classList.contains("espen")) {
+                // console.log('THL mouth')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("fv") && spns[i].classList.contains("espen")) {
                 espenSmile.classList.remove('showItem')
                 espenAEI.classList.remove('showItem')
                 espenNeutral.classList.remove('showItem')
@@ -196,8 +233,8 @@ function read() {
                 espenTHL.classList.remove('showItem')
                 espenFV.classList.add('showItem')
                 espenQW.classList.remove('showItem')
-                console.log('FV mouth')
-            } else if (spns[i].style.backgroundColor === "red" && spns[i].classList.contains("qw") && spns[i].classList.contains("espen")) {
+                // console.log('FV mouth')
+            } else if (spns[i].style.backgroundColor === highlight && spns[i].classList.contains("qw") && spns[i].classList.contains("espen")) {
                 espenSmile.classList.remove('showItem')
                 espenAEI.classList.remove('showItem')
                 espenNeutral.classList.remove('showItem')
@@ -205,7 +242,7 @@ function read() {
                 espenTHL.classList.remove('showItem')
                 espenFV.classList.remove('showItem')
                 espenQW.classList.add('showItem')
-                console.log('QW mouth')
+                // console.log('QW mouth')
             }
         }
 
@@ -216,33 +253,103 @@ function read() {
 
 // const audioClick = document.querySelector(".audioClick")
 
-startBtn.addEventListener('click', tstFunciton)
+startBtn.addEventListener('click', playAudio)
+pauseBtn.addEventListener('click', pauseAudio)
+resetBtn.addEventListener('click', startOver)
+pauseBtn.classList.add('hide')
 
-function tstFunciton() {
+function playAudio() {
     audi.play();
+    startBtn.classList.add('hide')
+    pauseBtn.classList.remove('hide')
+    pauseBtn.classList.add('active')
 }
 
-// audi.addEventListener("click", showText);
-// audioClick.addEventListener("click", showText);
+function pauseAudio() {
+    audi.pause();
+    startBtn.classList.remove('hide')
+    pauseBtn.classList.add('hide')
+    startBtn.classList.add('active')
+}
 
-// function showText() {
-//     const message0 = document.querySelector("message0")
-//     console.log(message0)
-//     setTimeout(function () {
-//         const message = document.querySelector("message0")
-//         message.classList.add('show')
-//         console.log('show message 0')
-//     }, 100)
+function startOver() {
+    spns.forEach(e => {
+        e.style.backgroundColor = "";
+    })
 
-//     setTimeout(function () {
-//         const message = document.querySelector("message1")
-//         message.classList.add('show')
-//         console.log('show message 1')
-//     }, 5500)
+    audi.currentTime = 0
+    audi.pause();
 
-//     setTimeout(function () {
-//         const message = document.querySelector("message2")
-//         message.classList.add('show')
-//         console.log('show message 2')
-//     }, 7500)
-// }
+    startBtn.classList.remove('hide')
+    pauseBtn.classList.add('hide')
+    pauseBtn.classList.remove('active')
+    startBtn.classList.remove('active')
+
+
+
+    smile.classList.add('showItem')
+    AEI.classList.remove('showItem')
+    Neutral.classList.remove('showItem')
+    O.classList.remove('showItem')
+    THL.classList.remove('showItem')
+    FV.classList.remove('showItem')
+    QW.classList.remove('showItem')
+    espenSmile.classList.add('showItem')
+    espenAEI.classList.remove('showItem')
+    espenNeutral.classList.remove('showItem')
+    espenO.classList.remove('showItem')
+    espenTHL.classList.remove('showItem')
+    espenFV.classList.remove('showItem')
+    espenQW.classList.remove('showItem')
+
+}
+
+
+// podcast info
+const podcastInfoBtn = document.querySelector('.podcastInfoClicker')
+const podcastInfo = document.querySelector('.infoWrapper')
+podcastInfoBtn.addEventListener('click', showInfo)
+
+function showInfo() {
+    podcastInfoBtn.classList.toggle('active')
+    podcastInfo.classList.toggle('active')
+}
+
+
+// EYES OPEN OR CLOSED 
+// BLINKING
+
+const vasilisEyesOpen = document.querySelector('.vasilisEyesOpen')
+const vasilisEyesClosed = document.querySelector('.vasilisEyesClosed')
+
+const espenEyesOpen = document.querySelector('.espenOpenEyes')
+const espenEyesClosed = document.querySelector('.espenClosedEyes')
+
+vasilisEyesOpen.classList.add('active')
+espenEyesOpen.classList.add('active')
+
+setInterval(function () {
+    // toggle the class every five second
+    vasilisEyesClosed.classList.toggle('active');
+    vasilisEyesOpen.classList.toggle('active');
+
+    setTimeout(function () {
+        // toggle back after 1 second
+        vasilisEyesClosed.classList.toggle('active');
+        vasilisEyesOpen.classList.toggle('active');
+    }, 300)
+
+}, 5000);
+
+setInterval(function () {
+    // toggle the class every five second
+    espenEyesClosed.classList.toggle('active');
+    espenEyesOpen.classList.toggle('active');
+
+    setTimeout(function () {
+        // toggle back after 1 second
+        espenEyesClosed.classList.toggle('active');
+        espenEyesOpen.classList.toggle('active');
+    }, 300)
+
+}, 7000);
